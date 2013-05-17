09e41e7927cf8709203898dd11aa39f62af86250
Nathaniel <natruf@gmail.com>
2013-05-16 23:22:08 -0500

    Going to bed now

--- Source/visual/windows/ViewProgram.java
+++ Source/visual/windows/ViewProgram.java
@@ -9,8 +9,8 @@ public class ViewProgram extends PApplet {
 	final int YSIZE = 400;
 	final String WINDOWNAME = "Program View";
 	
-	
-	
+	final int[] BGCOLOR = new int[] {120, 120, 120};
+	final int[] GRIDCOLOR = new int[] {0, 0, 0};
 	
 	public ViewProgram() {
 		this.runSketch();
@@ -19,14 +19,14 @@ public class ViewProgram extends PApplet {
 	public void setup() {
 		frame.setTitle(WINDOWNAME);
 		size(XSIZE, YSIZE);
-		background(120, 120, 120);
+		background(BGCOLOR[0], BGCOLOR[1], BGCOLOR[2]);
 	}
 	
 	public void draw() {
 		noStroke();
 		for(int a = 0; a <= XSIZE; a+=20) {
 			for(int b = 0; b <= YSIZE; b+=20) {
-				fill(0);
+				fill(GRIDCOLOR[0], GRIDCOLOR[1], GRIDCOLOR[2]);
 				ellipse(a, b, 2, 2);
 			}
 		}
--- Source/visual/windows/ViewToolkit.java
+++ Source/visual/windows/ViewToolkit.java
@@ -9,6 +9,10 @@ public class ViewToolkit extends PApplet{
 	final int YSIZE = 700;
 	final String WINDOWNAME = "Toolkit View";
 	
+	final int[] BGCOLOR = new int[] {120, 120, 120};
+	final int[] LINECOLOR = new int[] {0, 0, 0};
+
+	
 	public ViewToolkit() {
 		this.runSketch();
 	}
@@ -16,11 +20,18 @@ public class ViewToolkit extends PApplet{
 	public void setup() {
 		frame.setTitle(WINDOWNAME);
 		size(XSIZE, YSIZE);
-		background(200, 200, 0);
+		background(BGCOLOR[0], BGCOLOR[1], BGCOLOR[2]);
 	}
 	
 	public void draw() {
-		
+		noStroke();
+		fill(LINECOLOR[0], LINECOLOR[1], LINECOLOR[2]);
+		for(int a = 50; a < XSIZE; a+=50) {
+			rect(a-2, 0, 4, YSIZE);
+		}
+		for(int b = 100; b < YSIZE; b+=100) {
+			rect(0, b-2, XSIZE, 4);
+		}
 	}
 	
 }
